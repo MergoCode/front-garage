2013205ce65223f6a776576c15b122773f6c03b4
MergoCode <gen.nishant.1402@gmail.com@>
2024-12-29 00:39:15 +0200

    Added axios api and config.ts

--- frontend/src/NotFound.tsx
+++ frontend/src/NotFound.tsx
@@ -1,20 +1,27 @@
 import { useNavigate } from "react-router";
-import {useSelector, useDispatch} from "react-redux";
-import {RootState} from "./reduxStore/store";
-import { increment, decrement } from "./reduxStore/testreducer";
-import React from "react";
+import React, {useRef} from "react";
+import config from "./config";
+import { useCounterStore } from "./reduxStore/store";
 
 /* testing reducers (to be changed), typescript update */
 
 const NotFoundPage: React.FC = () => {4
-    const count = useSelector((state: RootState) => state.counter.value);
-    const dispatch = useDispatch();
-
+    const count = useCounterStore((state) => state.count);
+    const increment = useCounterStore((state) => state.increment);
+    const decrement = useCounterStore((state) => state.decrement);
     const navigate = useNavigate();
+    const inputRef = useRef<HTMLInputElement>(null);
+    const incrementByAmount = useCounterStore((state) => state.incrementByAmount);
+    const baseURL = config.apiBaseUrl;
 
 
     const handleRedirect = () => {
         navigate('/home');
+    }
+
+    const handleIncrementByAmount = () => {
+        const value = parseInt(inputRef.current?.value || "0", 10);
+        incrementByAmount(value);
     }
 
     return(
@@ -24,8 +31,12 @@ const NotFoundPage: React.FC = () => {4
             GO home 
         </button>
         <h1>{count}</h1>
-        <button onClick={() => dispatch(increment())}>increment</button>
-        <button onClick={() => dispatch(decrement())}>Decrement</button>
+        <input type="text" ref={inputRef}/>
+        <button onClick={increment}>increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={handleIncrementByAmount}>Increment by Input</button>
+        <h1>{baseURL}</h1>
+
     </div>);
 };
 

ce6eb482da74d769e6b249677ab69275820c2ee5
OlhaHupaliuk <olha.hupaliuk@lnu.edu.ua>
2025-04-10 22:24:58 +0300

    header fix

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -23,16 +23,17 @@ const Header = () => {
     return(
         <div className="header">
             <nav className="container px-0 header-nav">
-                <a className="col-2 header-link" href="#">
-                    <img src="/assets/kursova-icon.svg" alt='Курсова робота'></img>Курсова робота
-                </a>
 
                 <a className="col-2 header-link" href="#" onClick={() => navigate("/home")}>
                     <img src="/assets/home-icon.svg" alt='Головна сторінка'></img>Головна сторінка
                 </a>
 
-                <a className="col-2 header-link" onClick={()=> navigate('/audience-picker')}>
+                <a className="col-2 header-link" href="#">
                     <img src="/assets/schedule-icon.svg" alt='Розклад'></img>Розклад
+                </a>
+
+                <a className="col-2 header-link" onClick={()=> navigate('/audience-picker')}>
+                    <img src="/assets/kursova-icon.svg" alt='Вільні аудиторії'></img>Вільні аудиторії
                 </a>
 
                 <a className="col-2 header-link" href="#">
